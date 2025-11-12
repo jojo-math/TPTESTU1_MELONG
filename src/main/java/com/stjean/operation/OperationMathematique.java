c5d472f292b7e502d18a69ea2cfdf2713cd4e32e
MELONG TSAYEM Joan <joantsayem@gmail.com>
2025-11-12 05:25:54 +0100

    feat: trier() ordre decroissant

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -1,4 +1,12 @@
 package com.stjean.operation;
 
+import java.util.Arrays;
+
 public class OperationMathematique {
+    public Integer[] trier(Integer[] listes) {
+        if (listes == null) return new Integer[0];
+        Integer[] copy = Arrays.copyOf(listes, listes.length);
+        Arrays.sort(copy, (a, b) -> b - a); // décroissant
+        return copy;
+    }
 }
